0f9e5f3a02d119ad825f907de35c5eb829e9f72e
Crib406 <149419263+Crib406@users.noreply.github.com>
2025-11-11 11:23:36 +0100

    Workflow

--- .eleventy.js
+++ .eleventy.js
@@ -1,18 +1,14 @@
 module.exports = function (eleventyConfig) {
-  // 🔹 statische Dateien (Assets) unverändert kopieren
   eleventyConfig.addPassthroughCopy({ "src/assets": "assets" });
-
-  // 🔹 optional: zusätzliche statische Ordner (falls du mal brauchst)
-  // eleventyConfig.addPassthroughCopy({ "src/img": "img" });
-  // eleventyConfig.addPassthroughCopy({ "src/js": "js" });
 
   return {
     dir: {
-      input: "src",       // Quellordner
-      includes: "_includes", // Layouts & Partials
-      output: "_site"     // Zielordner für fertige Seite
+      input: "src",
+      includes: "_includes",
+      data: "_data",
+      output: "_site"   // wieder _site, nicht docs
     },
-    templateFormats: ["njk", "html", "md"], // welche Dateien Eleventy verarbeitet
-    htmlTemplateEngine: "njk"                // Engine für HTML-Dateien
+    templateFormats: ["njk", "html", "md"],
+    htmlTemplateEngine: "njk"
   };
 };
