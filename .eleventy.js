cbae14b566a68d1296ff66d174be9d740c99608a
Crib406 <149419263+Crib406@users.noreply.github.com>
2025-11-11 12:46:03 +0100

    Änderung lokal vs. Prod pfad

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,19 @@
 module.exports = function (eleventyConfig) {
+  // Umgebung ermitteln: local oder production
+  const env = process.env.ELEVENTY_ENV || "local";
+  const siteData = require(`./src/_data/site.${env}.json`);
+  eleventyConfig.addGlobalData("site", siteData);
+
+  // Statische Assets direkt durchschleifen
   eleventyConfig.addPassthroughCopy({ "src/assets": "assets" });
+  eleventyConfig.addPassthroughCopy("src/favicon.ico");
 
   return {
     dir: {
       input: "src",
       includes: "_includes",
       data: "_data",
-      output: "_site"   // wieder _site, nicht docs
+      output: "_site"
     },
     templateFormats: ["njk", "html", "md"],
     htmlTemplateEngine: "njk"
